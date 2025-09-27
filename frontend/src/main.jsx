aac0666ecd0766ab07d275eb11b8ec3716722f2c
tasneemsd <tasneembanu2005@gmail.com>
2025-09-27 22:18:14 +0530

    20:59

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css'
 
 
 
